a573cd83ded2f4659365964c09f7abcb274d9179
Gilkleyton Rodrigues <gilkleyton.andrade@gmail.com>
2025-04-13 22:41:47 -0300

    ajustes no sobre e consulta

---
+++ assets/js/sobre.js
@@ -0,0 +1,6 @@
+fetch('sobre.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('sobre-section').innerHTML = html;
+  })
+  .catch(err => console.error('Erro ao carregar o sobre:', err));
